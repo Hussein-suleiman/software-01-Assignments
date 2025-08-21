b57de90aee98db6a354ae8f269a956ff3b2037bd
HUSSEIN SULEIMAN <husseinsulley438@gmail.com>
2025-08-21 08:28:08 -0700

    I have completed module 2 (task01 to task06)

---
+++ module 2/Task04.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+num3 = int(input("Enter the third integer: "))
+_sum = num1 + num2 + num3
+product = num1 * num2 * num3
+average = _sum / 3
+print(f"Sum: {_sum}")
+print(f"Product: {product}")
+print(f"Average: {average:.2f}")
