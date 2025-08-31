c81be20289142b9c2e7892511b06a11c844d1d52
Hussein-suleiman <Husseinsulley438@gmail.com>
2025-08-31 12:36:44 +0100

    Delete module 4 directory

--- module 4/task02.py
+++
@@ -1,6 +0,0 @@
-while True:
-    inches = float(input("key in length in inches (negative to quit): "))
-    if inches < 0:
-        print("Program ended.")
-        break
-    print(f"{inches} inches = {inches * 2.54:.2f} cm")
